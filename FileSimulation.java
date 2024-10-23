6eabe2a92a5465a266eb3f9999b833117ba3ebed
jhalsey <julianhalsey5@gmail.com>
2024-10-23 12:57:09 -0400

    Added rm, rmdir, and pwd commands

--- FileSimulation.java
+++ FileSimulation.java
@@ -39,11 +39,11 @@ public class FileSimulation {
                             if ((nextNode != null) && ((nextNode.use).equals("Folder"))) {
                                 currentNode = nextNode;
                             } else {
-                                System.out.println("Directory not found.");
+                                System.out.println("Directory not found");
                             }
                         }
                     } else {
-                        System.out.println("No directory specified.");
+                        System.out.println("No directory specified");
                     } 
                     break;
                 case "file":
@@ -59,10 +59,27 @@ public class FileSimulation {
                     } 
                     break;
                 case "rm":
+                    if (arr.length > 1) {
+                        Node delNode = currentNode.findChildByName(second);
+                        if ((delNode != null) && !((delNode.use).equals("Folder"))) {
+                            currentNode.removeNode(delNode);
+                        } else {
+                            System.out.println("File not found or not file");
+                        }
+                    }
                     break;
                 case "rmdir":
+                    if (arr.length > 1) {
+                        Node delNode = currentNode.findChildByName(second);
+                        if ((delNode != null) && !((delNode.use).equals("File"))) {
+                            currentNode.removeNode(delNode);
+                        } else {
+                            System.out.println("Folder not found or not folder");
+                        }
+                    }
                     break;
                 case "pwd":
+                    currentNode.printWhere(currentNode);
                     break;
                 case "help":
                     break;
@@ -110,4 +127,22 @@ class Node {
         }
         return null;
     }
+
+    public void removeNode(Node node) {
+        children.remove(node);
+    }
+
+    public void printWhere(Node node) {
+        ArrayList<String> parents = new ArrayList<>();
+        Node parentN = node;
+        while (parentN != null) {
+            parents.add(parentN.name);
+            parentN = parentN.parent;
+        }
+        for (int i = parents.size() - 1; i >= 0; i--) {
+            System.out.print(parents.get(i) + "/");
+        }
+        System.out.println();
+    }
+
 }
